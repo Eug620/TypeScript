d7a55e43ff409d031c09433474510b5657133c25
yeyuhang <yeyuhang@appicplay.com>
2020-09-28 19:48:59 +0800

    feat(基础): 新增git-cz

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: yeyuhang
+ * @Date: 2020-09-28 19:16:47
+ * @LastEditTime: 2020-09-28 19:37:10
+ * @LastEditors: yeyuhang
+ * @Descripttion: 头部注释
+ */
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      // 把头部的大写关了
+    //   'header-case': [0, 'never', 72]
+    }
+}
+  
