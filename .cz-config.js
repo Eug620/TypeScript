46b268d769c453d150070fa56ce1f40960d9a63a
yeyuhang <yeyuhang@appicplay.com>
2020-09-28 19:51:07 +0800

    feat(package.json): 更修改一些配置

--- .cz-config.js
+++ .cz-config.js
@@ -1,32 +1,48 @@
 /*
- * @Author: yeyuhang
- * @Date: 2020-09-28 17:27:17
- * @LastEditTime: 2020-09-28 19:06:21
  * @LastEditors: yeyuhang
- * @Descripttion: 头部注释
  */
-'use strict';
- 
 module.exports = {
- 
   types: [
-    {value: '特性',     name: '特性:    一个新的特性'},
-    {value: '修复',      name: '修复:    修复一个Bug'},
-    {value: '文档',     name: '文档:    变更的只有文档'},
-    {value: '格式',    name: '格式:    空格, 分号等格式修复'},
-    {value: '重构', name: '重构:    代码重构，注意和特性、修复区分开'},
-    {value: '性能',     name: '性能:    提升性能'},
-    {value: '测试',     name: '测试:    添加一个测试'},
-    {value: '工具',    name: '工具:    开发工具变动(构建、脚手架工具等)'},
-    {value: '回滚',   name: '回滚:    代码回退'}
+    { value: 'feat', name: 'feat:           新增了一些特性' },
+    { value: 'fix', name: 'fix:            修复了一些Bug' },
+    { value: 'docs', name: 'docs:           只有文档的变动' },
+    {
+      value: 'style',
+      name: 'style:          只修改了代码的风格, 并不会对代码造成影响',
+    },
+    {
+      value: 'refactor',
+      name: 'refactor:       既无法修复错误也未添加功能的代码更改',
+    },
+    {
+      value: 'perf',
+      name: 'perf:           更改代码以提高性能',
+    },
+    { value: 'test', name: 'test:           添加缺失的测试' },
+    {
+      value: 'chore',
+      name:
+        'chore:          对构建过程或辅助工具和库的更改，例如文档生成',
+    },
+    { value: 'revert', name: 'revert:         对提交的代码回滚' },
+    { value: 'WIP', name: 'WIP:            工作正在进行中' },
   ],
- 
+
   scopes: [
-    {name: '基础'},
-    {name: '进阶'},
-    {name: '工程'}
+    { name: 'package.json' },
+    { name: 'all' },
+    { name: 'config-files' },
+    { name: '基础' },
+    { name: '进阶' },
+    { name: '工程' },
+    { name: '公共组件' }
   ],
- 
+
+  allowTicketNumber: false,
+  isTicketNumberRequired: false,
+  ticketNumberPrefix: 'TICKET-',
+  ticketNumberRegExp: '\\d{1,5}',
+
   // it needs to match the value for field type. Eg.: 'fix'
   /*
   scopeOverrides: {
@@ -40,21 +56,25 @@ module.exports = {
   */
   // override the messages, defaults are as follows
   messages: {
-    type: '选择一种你的提交类型:',
-    scope: '选择一个scope (可选):',
+    type: "Select the type of change that you're committing:",
+    scope: '\nDenote the SCOPE of this change (optional):',
     // used if allowCustomScopes is true
     customScope: 'Denote the SCOPE of this change:',
-    subject: '短说明:\n',
-    body: '长说明，使用"|"换行(可选)：\n',
-    breaking: '非兼容性说明 (可选):\n',
-    footer: '关联关闭的issue，例如：#31, #34(可选):\n',
-    confirmCommit: '确定提交说明?'
+    subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
+    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+    breaking: 'List any BREAKING CHANGES (optional):\n',
+    footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
+    confirmCommit: 'Are you sure you want to proceed with the commit above?',
   },
- 
+
   allowCustomScopes: true,
-  allowBreakingChanges: ['特性', '修复'],
- 
+  allowBreakingChanges: ['feat', 'fix'],
+  // skip any questions you want
+  skipQuestions: ['body'],
+
   // limit subject length
-  subjectLimit: 100
- 
+  subjectLimit: 100,
+  // breaklineChar: '|', // It is supported for fields body and footer.
+  // footerPrefix : 'ISSUES CLOSED:'
+  // askForBreakingChangeFirst : true, // default is false
 };
